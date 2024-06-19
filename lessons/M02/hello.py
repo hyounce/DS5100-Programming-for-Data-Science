563ac3cc5fba0e40b1f920890dda9b9ae9ba3cec
hyounce <hilde.younce@gmail.com>
2024-06-19 14:48:10 -0400

    Edited hello.py

--- lessons/M02/hello.py
+++ lessons/M02/hello.py
@@ -1,6 +1,8 @@
 print("hello world!")
-import this
 
 # Hilde - likes to watch movies
 # Dan - has been on a bunch of trips lately - zion for a wedding and white water rafting in grand canyon
 # Zach - from winchester, studied abroad in Madrid and keeps his phone in spanish to try to keep up
+
+import pandas as pd
+help(pd)
